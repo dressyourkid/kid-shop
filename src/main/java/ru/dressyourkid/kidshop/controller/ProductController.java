08764669679a2bb712737bacd563eda283261ff7
luckykeks <luckykeks@gmail.com>
2018-07-31 21:43:59 +0300

    Product list func

--- src/main/java/ru/dressyourkid/kidshop/controller/ProductController.java
+++ src/main/java/ru/dressyourkid/kidshop/controller/ProductController.java
@@ -1,13 +1,13 @@
 package ru.dressyourkid.kidshop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import ru.dressyourkid.kidshop.controller.exception.NotFoundException;
-import ru.dressyourkid.kidshop.model.ProductListItem;
+import ru.dressyourkid.kidshop.entity.StoreItem;
 import ru.dressyourkid.kidshop.model.ProductSingleView;
 import ru.dressyourkid.kidshop.service.ProductService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -17,8 +17,8 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("list")
-    public List<ProductListItem> listItems() { // todo see Pageable
-        return productService.list();
+    public Page<StoreItem> listItems(Pageable pageable) { // todo see Pageable
+        return productService.list(pageable);
     }
 
     @GetMapping("/{id}")
