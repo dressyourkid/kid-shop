26ad94e4e541578e5539e4e233bb1d1ccba39524
lconnected <levas1992@gmail.com>
2019-01-11 14:59:41 +0300

    Fetching page of products added

--- src/main/java/ru/dressyourkid/kidshop/controller/ProductController.java
+++ src/main/java/ru/dressyourkid/kidshop/controller/ProductController.java
@@ -2,18 +2,14 @@ package ru.dressyourkid.kidshop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.dressyourkid.kidshop.controller.exception.NotFoundException;
-import ru.dressyourkid.kidshop.model.ProductListItem;
 import ru.dressyourkid.kidshop.model.ProductSingleView;
 import ru.dressyourkid.kidshop.service.ProductService;
-
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -24,27 +20,12 @@ public class ProductController {
 
     @GetMapping
     public Page<ProductSingleView> listItems(Pageable pageable) { // todo see Pageable
-        List<ProductSingleView> results = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            ProductSingleView product = new ProductSingleView();
-            product.setId((long) i);
-            product.setName("name one");
-            product.setDescription("Very long text with product description");
-            product.setPrice(new BigDecimal(100L));
-            results.add(product);
-        }
-        return new PageImpl<>(results);
+        return productService.findPage(pageable);
     }
 
     @GetMapping("/{id}")
     public ProductSingleView singleItem(@PathVariable("id") Long productId) throws NotFoundException { // todo see Pageable
         return productService.fetchProduct(productId);
-//        ProductSingleView product = new ProductSingleView();
-//        product.setId(productId);
-//        product.setName("name one");
-//        product.setDescription("Very long text with product description");
-//        product.setPrice(new BigDecimal(100L));
-//        return product;
     }
 
 }
--- src/main/java/ru/dressyourkid/kidshop/service/ProductService.java
+++ src/main/java/ru/dressyourkid/kidshop/service/ProductService.java
@@ -5,19 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import ru.dressyourkid.kidshop.controller.exception.NotFoundException;
-
 import ru.dressyourkid.kidshop.entity.ProductStore;
-import ru.dressyourkid.kidshop.model.ProductInfoDto;
-import ru.dressyourkid.kidshop.model.ProductListItem;
 import ru.dressyourkid.kidshop.model.ProductSingleView;
-import ru.dressyourkid.kidshop.repository.ProductMetaRepository;
 import ru.dressyourkid.kidshop.repository.ProductStoreRepository;
-
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * Created by lconnected on 24/07/2018.
@@ -26,23 +16,22 @@ import java.util.stream.Collectors;
 public class ProductService {
 
     @Autowired
-    private ProductMetaRepository productMetaRepository;
-
-    @Autowired
     private ProductStoreRepository productStoreRepository;
 
+    public Page<ProductSingleView> findPage(Pageable pageable) {
+        Page<ProductStore> productStorePage = productStoreRepository.findAll(pageable);
+        Page<ProductSingleView> productPage = productStorePage
+                .map(entity -> {
+                    ProductSingleView product = new ProductSingleView();
+                    product.setId(entity.getId());
+                    product.setName(entity.getProductMeta().getName());
+                    product.setDescription(entity.getProductMeta().getDescription());
+                    product.setPrice(entity.getPrice());
+                    return product;
+                });
+        return productPage;
+    }
 
-
-//
-//
-//    public List<ProductListItem> findAll(Pageable pageable) {
-//        Page<StoreItem> storeItems = storeRepository.findAll(pageable);
-//        List<ProductListItem> productListItems = storeItems.stream().map(entity ->
-//                new ProductListItem(entity.getProduct().getName(),entity.getItemPrice()))
-//                .collect(Collectors.toList());
-//     return productListItems;
-//    }
-//
     public ProductSingleView fetchProduct(Long productId) throws NotFoundException {
 
         ProductStore productStoreItem = productStoreRepository
